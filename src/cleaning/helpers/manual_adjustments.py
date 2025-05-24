377035eaec930dc1d2e3f313cb748ee6dc8889f7
Paul Ebert <paul.ebert@code.berlin>
2025-05-24 21:34:29 +0200

    manual adjustments func

---
+++ src/cleaning/helpers/manual_adjustments.py
@@ -0,0 +1,13 @@
+import re
+
+def manual_adjustments(title):
+    title = re.sub(r'\bmoll\b', 'minor', title)
+    title = re.sub(r'\bmin\b', 'minor', title)
+    title = re.sub(r'\bbb\b', 'b flat', title, flags=re.IGNORECASE)
+    title = re.sub(r'\b\d*\/\d+\b', lambda x: x.group(0).replace('/', ' no '), title)
+    title = re.sub(r'\bin 3\b', 'in e', title)
+    title = re.sub(r'\besp\b', 'espagnole', title, flags=re.IGNORECASE)
+    title = re.sub(r'\bspanish\b', 'espagnole', title, flags=re.IGNORECASE)
+    # title = re.sub(r'\b\(?complete\)?\b', '', title, flags=re.IGNORECASE)
+
+    return title
