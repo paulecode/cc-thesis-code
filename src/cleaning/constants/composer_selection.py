8d52ae5a69d0d8a9b96b0449b22955d916d49f7b
Paul Ebert <paul.ebert@code.berlin>
2025-05-24 21:32:18 +0200

    add composer selection

---
+++ src/cleaning/constants/composer_selection.py
@@ -0,0 +1,11 @@
+# Use these composers only
+composer_selection_list = ['Frédéric Chopin', 'Franz Schubert', 'Ludwig van Beethoven', 'Johann Sebastian Bach', 'Franz Liszt']
+
+# Rename to more common spellings
+composer_mapping_dict = {
+'Frédéric Chopin': 'Chopin',
+'Ludwig van Beethoven': 'Beethoven',
+'Johann Sebastian Bach': 'Bach',
+'Franz Schubert': 'Schubert',
+'Franz Liszt': 'Liszt'
+}
