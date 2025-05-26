6bb4f3cf3e0b903b2059d6d306c2ce066195e454
Paul Ebert <paul.ebert@code.berlin>
2025-05-26 11:33:57 +0200

    add more options and extract vars

--- src/dashboard/components/piece_selection_container.py
+++ src/dashboard/components/piece_selection_container.py
@@ -9,6 +9,10 @@ def select_piece(df, id):
     st.write(f"Selection {id}")
     segments = True
     chart_selection = None
+    chart_selection_options = ['None', 'RMS', 'Waveform', 'Spectrogram', 'Mel-Spectrogram', 'MFCC', 'STFT', 'FFT']
+    normalize = True
+    scrollable = True
+
     selection = select_piece_list(df, key=f"dfs{id}")
     if selection and 'selection' in selection and 'rows' in selection['selection'] and selection['selection']['rows']:
         index = selection['selection']['rows'][0]
@@ -24,10 +28,20 @@ def select_piece(df, id):
                     segments = True
                 else:
                     segments = False
-                chart_selection = st.selectbox("Which chart to display", options=['None', 'RMS', 'Waveform', 'Spectrogram', 'Mel-Spectrogram', 'MFCC', 'STFT', 'FFT'], key=f"cs{id}")
+                chart_selection = st.selectbox("Which chart to display", options=chart_selection_options, key=f"cs{id}")
+                normalize_yes_or_no = st.radio("Normalize", ["Yes", "No"], horizontal=True, key=f"na{id}")
+                if normalize_yes_or_no == "Yes":
+                    normalize = True
+                else:
+                    normalize = False
+                scrollable_container = st.radio("Scrollable container", ["Yes", "No"], horizontal=True, key=f"sc{id}")
+                if scrollable_container == "Yes":
+                    scrollable = True
+                else:
+                    scrollable = False
 
         if midi_or_wav == "MIDI":
             midi_section(row)
 
         if midi_or_wav == "WAV":
-            wav_section_container(row, segments, chart_selection, id)
+            wav_section_container(row, normalize, segments, chart_selection, scrollable, id)
